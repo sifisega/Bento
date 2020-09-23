22ffa0e8d2de722e64d4f441db403afe70abf6e5
sifisega <50163149+sifisega@users.noreply.github.com>
2020-09-24 07:34:28 +1200

    Location added

--- js/weather.js
+++ js/weather.js
@@ -21,8 +21,8 @@ setPosition();
 function setPosition(position) {
     // Here you can change your position
     // You can use https://www.latlong.net/ to get it! (I use San Francisco as an example)
-    let latitude = 37.774929;
-    let longitude = -122.419418;
+    let latitude = -43.530800;
+    let longitude = 172.638000;
 
     getWeather(latitude, longitude);
 }
