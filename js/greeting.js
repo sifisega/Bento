6e1264ec8ababe9d344d350d2dbe73525a71b9b3
sifisega <50163149+sifisega@users.noreply.github.com>
2020-09-24 07:42:08 +1200

    Personalised

--- js/greeting.js
+++ js/greeting.js
@@ -2,18 +2,22 @@ var today = new Date();
 var Hr = today.getHours();
 
 // Here you can change the greetings and your name
-var name = 'John Doe';
-var lateTxt = 'Go to Sleep! ';
-var morningTxt = 'Good morning! ';
-var afterTxt = 'Good afternoon ';
+var mrname = 'Mr. Hall';
+var name = 'Silas';
+var lateTxt = '! Go to sleep!';
+var morningTxt = 'Good morning ';
+var dayTxt = 'Good day';
+var afterTxt = 'Afternoon ';
 var evenTxt = 'Good evening ';
 
-if (Hr >= 0 && Hr < 6) {
-    document.getElementById('greetings').innerText = lateTxt + name;
-} else if (Hr >= 6 && Hr < 12) {
+if (Hr >= 0 && Hr < 5) {
+    document.getElementById('greetings').innerText = name + lateTxt;
+} else if (Hr >= 5 && Hr < 10) {
     document.getElementById('greetings').innerText = morningTxt + name;
-} else if (Hr >= 12 && Hr < 17) {
+} else if (Hr >= 10 && Hr < 2) {
+    document.getElementById('greetings').innerText = dayTxt + mrname;
+} else if (Hr >= 2 && Hr < 17) {
     document.getElementById('greetings').innerText = afterTxt + name;
 } else {
-    document.getElementById('greetings').innerText = evenTxt + name;
+    document.getElementById('greetings').innerText = evenTxt + mrname;
 }
